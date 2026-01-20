24015a7e2ac815681f87780471c7d96ca2fac976
Diamantis Akmanidis <diama.akmanidis@reedonline.co.uk>
2026-01-20 18:08:19 +0000

    20260120

--- scripts/staging_meta/170_row_iteration.sql
+++ scripts/staging_meta/170_row_iteration.sql
@@ -1,20 +1,21 @@
--- NEW_COLUMN_NAME 
+-- 302 -- NEW_COLUMN_NAME 
 UPDATE MS_RAW.STG_META.SF_COLUMNS
    SET NEW_COLUMN_NAME = CASE WHEN COLUMN_NAME  = 'TIME_STAMP'
                               THEN '_ROW_ITERATION'
                               ELSE '_' || COLUMN_NAME
                          END
 WHERE COLUMN_NAME ILIKE ANY ('%ROW_ITERATION','TIME_STAMP')
+  AND DATA_TYPE = 'BINARY'
 ;
 
-
+-- 302
 INSERT INTO MS_RAW.STG_META.SF_COLUMNS
       (TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, NEW_TABLE_NAME,
        NEW_COLUMN_NAME, NEW_COLUMN_EXPRESSION, DATA_TYPE) 
 SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, NEW_TABLE_NAME,
        NEW_COLUMN_NAME || '_NUM' AS NEW_COLUMN_NAME, 
        'TO_NUMBER(TO_VARCHAR(' || COLUMN_NAME || '), ''XXXXXXXXXXXXXXXX'')' AS NEW_COLUMN_EXPRESSION,
-       DATA_TYPE
+       'NUMBER' AS DATA_TYPE
 FROM MS_RAW.STG_META.SF_COLUMNS
 WHERE DATA_TYPE = 'BINARY'
   AND NEW_COLUMN_NAME ILIKE '%ROW_ITERATION'
@@ -48,10 +49,11 @@ select
 --from rol_raw.reedonline_dbo.job_search_alert
 from rol_raw.reedonline_dbo_frequent.job_search_alert
 limit 1;
-*/
+
 
 -- for ALL the rest -> row_iteration -> row_iteration_num
 select
     to_number(to_varchar(row_iteration), 'XXXXXXXXXXXXXXXX') as row_iteration_num,  --BEST
 from rol_raw.reedonline_dbo.users
 limit 1;
+*/
